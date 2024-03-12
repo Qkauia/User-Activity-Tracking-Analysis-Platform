cbb926c691dca1ad490049aae300a89fab6f3d19
艸先生 <kauia96@gmail.com>
2024-03-13 03:06:53 +0800

    feat:add devise

---
+++ db/migrate/20240312183733_add_sign_activity_count_to_users.rb
@@ -0,0 +1,7 @@
+class AddSignActivityCountToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :sign_out_count, :integer, default: 0, null: false
+    add_column :users, :registration_count, :integer, default: 0, null: false
+    add_column :users, :reset_password_count, :integer, default: 0, null: false
+  end
+end
