095c0bd26ae9031b4599ccbe2af8bd90195a055c
艸先生 <kauia96@gmail.com>
2024-03-13 15:39:14 +0800

    feat:Activity Model validate, Activity Controller 基本CRUD

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,17 +1,52 @@
 # frozen_string_literal: true
 
 class ActivitiesController < ApplicationController
-  def new; end
+  before_action :find_activity,only: [ :show, :edit, :update, :destroy]
+  
+  def new
+    @activity = Activity.new
+  end
 
-  def create; end
+  def create
+    @activity = Activity.new(activity_params)
+    if @activity.save
+      redirect_to activities_path, notice: '活動建立成功'
+    else
+      redirect_to :new,alert: '輸入不正確'
+    end
+  end
 
-  def show; end
+  def show 
+  end
 
-  def edit; end
+  def edit 
+  end
 
-  def update; end
+  def update
+    if @activity.update(activity_params)
+      redirect_to activity_path(@activity), notice: '活動更新成功'
+    else
+      redirect_to :edit
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @activity.destroy
+    redirect_to activities_path, notice: '活動已經刪除'
+  end
 
-  def index; end
+  def index
+    @activities = Activity.all
+  end
+
+  private
+
+  def find_activity
+    @activity = Activity.find(params[:id])
+  end
+
+  def activity_params
+    params.require(:activity).permit(:title, :description, :start_time, :end_time, :location, :organizer, :status)
+  end
+
 end
