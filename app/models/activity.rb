3e084bc95550784aaae552572240d3df61c61089
艸先生 <kauia96@gmail.com>
2024-03-13 15:39:14 +0800

    feat:Activity Model validate, Activity Controller 基本CRUD

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,19 @@
 class Activity < ApplicationRecord
   has_many :bookings
   has_many :logs
+
+  validates :title, :district, :start_time, :end_time, :location, :organizer, presence: true
+  validates :max_participants, presence: true, numericality: { greater_than: 5, only_integer: true }
+  validate :end_time_greater_than_start_time
+
+  private
+
+  def end_time_greater_than_start_time
+    return if end_time.blank? || start_time.blank?
+
+    if end_time <= start_time
+      errors.add(:end_time, "必須晚於開始時間")
+    end
+  end
+
 end
