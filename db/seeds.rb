6a12e6ab4fd3c87f7b25514da1a9988da054d0ee
艸先生 <kauia96@gmail.com>
2024-03-13 11:20:41 +0800

    feat: devise add admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "建立資料中..."
+
+User.create!(email: 'admin@test.com', password: '111111', password_confirmation: '111111', admin: true)
+
+puts "系統管理員<<#{Admin.email}>>建立完成！"
