2363cb6107c11a59cbd46a05c639e88c454ba02a
艸先生 <kauia96@gmail.com>
2024-03-16 22:48:16 +0800

    fix:change User column add name column, delete count cloumn

---
+++ db/migrate/20240316144238_remove_conut_column_to_user.rb
@@ -0,0 +1,8 @@
+class RemoveConutColumnToUser < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :users, :sign_in_count
+    remove_column :users, :sign_out_count
+    remove_column :users, :reset_password_count
+    remove_column :users, :registration_count
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_15_132443) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_16_144238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,15 +73,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_15_132443) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "sign_out_count", default: 0, null: false
-    t.integer "registration_count", default: 0, null: false
-    t.integer "reset_password_count", default: 0, null: false
     t.boolean "admin"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
