00e72b7dfcb4a725b351991429ee2ac215dc4f78
艸先生 <kauia96@gmail.com>
2024-03-20 22:39:32 +0800

    test: add Activity feature

--- spec/factories/activities.rb
+++ spec/factories/activities.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :activity do
     title { "默默會" }
     description { "五倍學員聚聚" }
-    start_time { Time.zone.today }
-    end_time { 5.days.since }
+    start_time { Time.current + 1.day }
+    end_time { 5.days.from_now }
     location { "五倍紅寶石" }
     max_participants { 20 }
     organizer { "艾瑞克集團" }
