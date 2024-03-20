70af347258ad46724b2d512b3310b1094d87fec8
艸先生 <kauia96@gmail.com>
2024-03-20 22:38:15 +0800

    fix: wheneven mailer 環境調整

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
-every :day, at: '8:00 am' do
-  runner 'SummaryMailer.send_daily_summary'
+rails_env = ENV['RAILS_ENV'] || :development
+set :environment, rails_env
+set :output, "log/cron_log.log"
+env :PATH, ENV['PATH']
+
+every :day, at: '8:00' do
+  runner 'SummaryMailer.send_daily_summaries'
 end
 
-every :monday, at: '8:00 am' do
-  runner 'SummaryMailer.send_weekly_summary'
+every :monday, at: '8:00' do
+  runner 'SummaryMailer.send_weekly_summaries'
 end
 
-every :sunday, at: '4:30 am' do
+every :sunday, at: '4:30' do
   runner 'CleanOldLogsJob.perform_later'
 end
 
